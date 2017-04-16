09fe03e931bd10740550dc478f4101fc747d2c91
mjrulesamrat <mjrulesamrat@gmail.com>
2017-04-16 13:12:27 +0530

    Go defer, panic, recover example

---
+++ demos/recover.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func doPanic() {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Println("Recover with: ", e)
+		}
+	}()
+	panic("Just panicking for the sake of demo")
+	fmt.Println("This line will never be called")
+}
+
+func main() {
+	fmt.Println("Starting to panic..")
+	doPanic()
+	fmt.Println("Program regains control after panic recover")
+}
