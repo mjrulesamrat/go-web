e5ba802b20d07ce833eba35ea3f839148a1d6ab3
mjrulesamrat <mjrulesamrat@gmail.com>
2017-04-27 00:23:22 +0530

    unbuffered channel with two gorotines

--- concurrency/unbuffered_channels.go
+++ concurrency/unbuffered_channels.go
@@ -1,26 +1,38 @@
 package main
 
 import "fmt"
+import "sync"
+
+var wg sync.WaitGroup
 
 func main() {
 
 	count := make(chan int)
 
-	go PrintCounts(count)
+	wg.Add(2)
+	fmt.Println("Start Goroutines")
+	go PrintCounts(count, "A")
+	go PrintCounts(count, "B")
 	fmt.Println("Channel begins")
 	count <- 1
+	
+	fmt.Println("Waiting to finish")
+	wg.Wait()
+	fmt.Println("\n Termination Program")
 }
 
-func PrintCounts(count chan int) {
+func PrintCounts(count chan int, label string) {
+	// Schedule the call to WaitGroup's Done to tell we are done.
+	defer wg.Done()
 	for {
 		val, ok := <-count
 		if !ok {
 			fmt.Println("Channel was closed \n")
 			return
 		}
-		fmt.Printf("Count: %d received \n", val)
+		fmt.Printf("Count: %d received from label %s \n", val, label)
 		if val == 10 {
-			fmt.Println("Channel closed \n")
+			fmt.Printf("Channel closed from label %s \n", label)
 			close(count)
 			return
 		}
